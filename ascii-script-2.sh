599d1222bab5b902cd0f0fa1f72707e1ccc5d446
arupgitHubB <163707806+arupgitHubB@users.noreply.github.com>
2026-05-23 18:01:46 +0530

    modify shell script

--- ascii-script-2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-sudo apt-get install cowsay -y
-cowsay -f tux "Hello from the terminal!" >> tux.txt
-grep -i "Hello" tux.txt
-cat tux.txt
-ls -ltra
--- ascii-script.sh
+++ ascii-script.sh
@@ -3,4 +3,8 @@ sudo apt-get install cowsay -y
 cowsay -f dragon "Run for cover. I am a DRAGON.. RAWR !!" >> dragon.txt
 grep -i "dragon" dragon.txt
 cat dragon.txt
+
+## 2nd image for fun
+cowsay -f tux "Hello from the terminal!" >> tux.txt
+cat tux.txt
 ls -ltra
